59635aaf1bd33dac6a111f44383403cb4f149ec6
Miguel Muscat <mail@miguelmuscat.me>
2026-01-10 14:26:21 +0100

    code cleanup

--- views/views.go
+++ views/views.go
@@ -10,7 +10,8 @@ import (
 
 var TemplateFS fs.FS
 var cache = map[string]*t.Template{}
-const baseTmpl = "base.gotmpl"
+
+const baseTmplFilepath = "base.gotmpl"
 
 // Writes a view template to a writer with the given data. Failure will not be tolerated.
 func Write(filename string, w io.Writer, data any) {
@@ -23,20 +24,20 @@ func Write(filename string, w io.Writer, data any) {
 // Retrieves the template object for a template file, consulting the cache first
 // and populating it if missing for the given file.
 func getTemplate(filepath string) *t.Template {
-	if tmpl, isCached := cache[filepath]; isCached {
-		return tmpl
+	tmpl, isCached := cache[filepath]
+	if !isCached {
+		tmpl = createTemplate(filepath)
+		cache[filepath] = tmpl
 	}
-	tmpl := createTemplate(filepath)
-	cache[filepath] = tmpl
 	return tmpl
 }
 
 // Creates a template object from a template file
 func createTemplate(filepath string) *t.Template {
-	if filepath == baseTmpl {
-		return t.Must(t.New("base.gotmpl").Funcs(funcMap).ParseFS(TemplateFS, baseTmpl))
+	if filepath == baseTmplFilepath {
+		return t.Must(t.New("base.gotmpl").Funcs(funcMap).ParseFS(TemplateFS, baseTmplFilepath))
 	} else {
-		base := getTemplate(baseTmpl)
+		base := getTemplate(baseTmplFilepath)
 		tmpl := t.Must(t.Must(base.Clone()).ParseFS(TemplateFS, filepath))
 		return tmpl
 	}
@@ -44,7 +45,7 @@ func createTemplate(filepath string) *t.Template {
 
 // Clears a single parsed view template from the cache
 func ClearCache(filepath string) {
-	if filepath == baseTmpl {
+	if filepath == baseTmplFilepath {
 		ClearAllCache()
 	} else {
 		delete(cache, filepath)
